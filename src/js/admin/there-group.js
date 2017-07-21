0dce4a1f5a74d5b69a05d76dc8b682ec0d31e4b9
Pahnki Kim <pahnki.kim@pkworks.io>
2017-07-21 12:50:44 +0900

    update there-group api binding

--- src/js/admin/there-group.js
+++ src/js/admin/there-group.js
@@ -1,3 +1,23 @@
 require('../../less/admin/there-group.less');
 
+var _ = require('lodash');
 var common = require('./common');
+
+$.ajax({
+    url: '/api/admin/there/groups',
+    success: function(result) {
+        setList(result);
+    }
+});
+
+function setList(groups) {
+    var thereGroupsTemplate = require('../../template/admin/there-groups.hbs');
+    var thereGroupsHtml = thereGroupsTemplate(groups);
+
+    $('.hta-there-groups tbody').html(thereGroupsHtml);
+
+    $('.hta-total-there-groups').text(groups.length);
+    $('.hta-total-theres').text(_.reduce(groups, function(total, group) {
+        return total + group.thereCount;
+    }, 0));
+}
