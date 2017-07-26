a4c42ce647d7db7daa75bd3b1c5b3ff623702eef
Pahnki Kim <pahnki.kim@pkworks.io>
2017-07-26 12:25:53 +0900

    update there-info template & style

--- src/js/admin/there-info.js
+++ src/js/admin/there-info.js
@@ -1,3 +1,20 @@
 require('../../less/admin/there-info.less');
 
 var common = require('./common');
+
+$.ajax({
+    url: '/api/admin/there/list',
+    data: {
+        groupId: 'America'
+    },
+    success: function(result) {
+        setList(result);
+    }
+});
+
+function setList(theres) {
+    var theresTemplate = require('../../template/admin/theres.hbs');
+    var theresHtml = theresTemplate(theres);
+
+    $('.hta-there-list').html(theresHtml);
+}
