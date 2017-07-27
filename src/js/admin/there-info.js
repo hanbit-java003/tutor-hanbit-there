370b25f48d236e70d87f17cc993496912d98eb5e
Pahnki Kim <pahnki.kim@pkworks.io>
2017-07-27 11:12:18 +0900

    update there-info search

--- src/js/admin/there-info.js
+++ src/js/admin/there-info.js
@@ -1,6 +1,9 @@
 require('../../less/admin/there-info.less');
+var _ = require('lodash');
 
 var common = require('./common');
+
+var thereList = [];
 
 $.ajax({
     url: '/api/admin/there/groups',
@@ -26,6 +29,8 @@ function requestList(groupId) {
             groupId: groupId
         },
         success: function(result) {
+            thereList = result;
+
             setList(result);
         }
     });
@@ -36,4 +41,42 @@ function setList(theres) {
     var theresHtml = theresTemplate(theres);
 
     $('.hta-there-list').html(theresHtml);
+}
+
+$('#hta-there-search-input').on('paste cut', function(event) {
+    setTimeout(search, 100);
+});
+
+$('#hta-there-search-input').on('keyup', function(event) {
+    switch (event.keyCode) {
+        case 27:    // esc
+            $('#hta-there-search-input').val('');
+        case 8:     // backspace
+        case 46: {  // delete
+            search();
+            break;
+        }
+    }
+});
+
+$('#hta-there-search-input').on('keypress compositionend', function() {
+    search();
+});
+
+function search() {
+    var keyword = _.kebabCase($('#hta-there-search-input').val().toLowerCase());
+
+    thereList.forEach(function(there) {
+        var id = there.id.toLowerCase();
+        var name = there.name.toLowerCase();
+
+        if (id.includes(keyword) || name.indexOf(keyword) > -1) {
+            delete there.hidden;
+        }
+        else {
+            there.hidden = true;
+        }
+    });
+
+    setList(thereList);
 }
