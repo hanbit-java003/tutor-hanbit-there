c161da93a00ed8287418900c67d495b54abaec56
Pahnki Kim <pahnki.kim@pkworks.io>
2017-08-08 12:21:55 +0900

    update list template

--- src/js/activities.js
+++ src/js/activities.js
@@ -7,6 +7,10 @@ var carousel = require('./ht-carousel');
 
 var model = {
     name: '괌 코코팜 가든 비치',
+    group: {
+        nameEn: 'Guam',
+        background: '/api/file/there-Guam'
+    },
     photos: [
         '../img/activities/guam-dolphin-cruise01.jpg',
         '../img/activities/guam-dive-experience01.jpg',
@@ -16,10 +20,58 @@ var model = {
     location: {
         lat: 13.628359,
         lng: 144.840244
-    }
+    },
+    intro: '자연 그대로의 모습을 간직하고있는 북부 리티디안 비치에서는 투몬비치와는 다른 투명함을 느낄수 있답니다.<br>\n' +
+        '리티디안 비치가 험난한 도로 때문에 가기 힘들었다면 코코팜 가든 비치를 방문해보세요.<br>\n' +
+        '같은 리티디안에 있는 코코팜 가든은 괌 뚜벅이 여행자도 손쉽게 이용 가능합니다.<br>\n' +
+        '깊이 들어가지 않아도 수많은 산호와 물고기를 볼 수 있는, 해변에서 시간을 보내기에 필요한 모든 것이 갖춰져 있는 곳 괌의 지상 낙원입니다.',
+    lists: [{
+        title: '바우처 안내',
+        type: 'icon',
+        items: [{
+            title: '티켓 형태',
+            icon: 'tags',
+            text: '예약 확정 E-메일(캡쳐가능)'
+        }, {
+            title: '발권소요시간',
+            icon: 'clock-o',
+            text: '24시간(영업일 기준)'
+        }]
+    }, {
+        title: '안내 사항',
+        type: 'dot',
+        items: [{
+            text: '최소 이용 예정 2일 전 예약주셔야 진행이 가능합니다.'
+        }, {
+            text: '금요일 오후 4시 이후,주말 예약 또한 시스템을 통해 문제 없이 접수됩니다. 다만, 현지 업체가 휴일인 관계로 차주 월요일부터 순차적으로 처리됨을 양해 부탁드립니다.'
+        }, {
+            text: '본 상품은 자유여행객을 위한 상품으로 타 여행사의 패키지 및 세미 패키지 이용 고객님은 이용하실 수 없습니다.'
+        }, {
+            text: '예약시 고객님께서 잘못 입력하신 정보(연락처, 이름, 이메일 주소 등) 및 분실에 따른 불이익에 대한 책임을 지지 않습니다.'
+        }]
+    }, {
+        title: '변경 및 환불 규정',
+        type: 'dot',
+        items: [{
+            text: '문의 사항은 영업시간 월-금 09:30 - 18:00(주말 및 공휴일 제외)내에 유선(1661-0882) 또는 카카오톡(ID검색 : @there)으로 요청해 주세요.'
+        }, {
+            text: '카카오톡(ID검색 : @there) 상담은 월-일 09:30 - 18:30 주말 및 공휴일에도 상담 가능합니다.'
+        }, {
+            text: '발권 진행 중 변경/환불시 수수료가 부과되거나 변경/환불이 불가할 수 있습니다.'
+        }, {
+            text: '결제 후 변경/환불에 대해서는 천재지변을 제외하고 취소 수수료가 발생하거나 변경/환불이 불가할 수 있습니다.'
+        }, {
+            text: '상품 이용 당일 취소나 불참(NO-SHOW)시에도 역시 환불이 불가합니다.'
+        }]
+    }]
 }
 
 function initActivity(model) {
+    $('.ht-activity-name').html(model.name);
+    $('.ht-activity-in').html('in ' + model.group.nameEn);
+    $('.ht-section-activity').css('background-image',
+        'url(' + model.group.background + ')');
+
     carousel.init($('.ht-activity-photos'),
         model.photos,
         function(slide) {
@@ -65,6 +117,16 @@ function initActivity(model) {
         $googleMaps.event.trigger(areaMap, 'resize');
         areaMap.panTo(model.location);
     });
+
+    $('.ht-activity-intro-text').html(model.intro);
+
+    var listTemplate = require('../template/activities/info-list.hbs');
+
+    for (var i=0; i<model.lists.length; i++) {
+        var listHtml = listTemplate(model.lists[i]);
+
+        $('.ht-activity-info-text').append(listHtml);
+    }
 }
 
 initActivity(model);
