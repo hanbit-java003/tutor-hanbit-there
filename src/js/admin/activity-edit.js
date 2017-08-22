21112630e4ee08723d9101cdee23c5a3aee67197
Pahnki Kim <pahnki.kim@pkworks.io>
2017-08-22 12:24:11 +0900

    update hasActivityId api

--- src/js/admin/activity-edit.js
+++ src/js/admin/activity-edit.js
@@ -4,3 +4,85 @@ var UrlSearchParams = require('url-search-params');
 var params = new UrlSearchParams(location.search);
 
 var common = require('./common');
+
+var model = {};
+var validId = false;
+
+$.ajax({
+    url: '/api/admin/there/groups',
+    success: function(result) {
+        var thereGroupItemsTemplate = require('../../template/admin/there-group-items.hbs');
+        var thereGroupItemsHtml = thereGroupItemsTemplate(result);
+
+        $('#hta-there-group-select .dropdown-menu').html(thereGroupItemsHtml);
+
+        $('#hta-there-group-select .dropdown-menu a').on('click', function(event) {
+            common.addDropdownEvent(event, this);
+
+            $('#hta-there-select .dropdown-title').html('지역');
+            $('#hta-there-select .dropdown-menu').empty();
+            $('.hta-activity-list').empty();
+            delete model.thereId;
+
+            var groupId = $(this).attr('group-id');
+            requestTheres(groupId);
+        });
+    }
+});
+
+function requestTheres(groupId) {
+    $.ajax({
+        url: '/api/admin/there/list',
+        data: {
+            groupId: groupId
+        },
+        success: function(result) {
+            var thereItemsTemplate = require('../../template/admin/there-items.hbs');
+            var thereItemsHtml = thereItemsTemplate(result);
+
+            $('#hta-there-select .dropdown-menu').html(thereItemsHtml);
+
+            $('#hta-there-select .dropdown-menu a').on('click', function(event) {
+                common.addDropdownEvent(event, this);
+
+                var thereId = $(this).attr('there-id');
+                model.thereId = thereId;
+            });
+        }
+    });
+}
+
+$('#hta-activity-id').on('change', function() {
+    validId = false;
+});
+
+$('.hta-check-duplicate').on('click', function() {
+    var id = $('#hta-activity-id').val().trim();
+
+    if (!id) {
+        alert('액티비티ID를 입력하세요.');
+        $('#hta-activity-id').focus();
+        return;
+    }
+    else if (!/^[a-zA-Z][0-9a-zA-Z\-]{3,}$/.test(id)) {
+        alert('잘못된 ID형식입니다.');
+        $('#hta-activity-id').focus();
+        return;
+    }
+
+    $.ajax({
+        url: '/api/admin/activity/' + id,
+        method: 'OPTIONS',
+        success: function(result) {
+            if (!result.exists) {
+                alert('사용할 수 있는 ID입니다.');
+                validId = true;
+            }
+            else {
+                alert('사용할 수 없는 ID입니다.');
+                $('#hta-activity-id').focus();
+                validId = false;
+            }
+        }
+    });
+});
